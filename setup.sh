9ece61c4346295a2f23730e42ea9667384e069a4
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-10-19 14:13:44 +0900

    Add link

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#
+# https://github.com/teru01/toytcp/blob/b8ae46590011d528a63325e800fce142e16139fc/setup.sh
+#
 # Copyright 2020 Teruya Ono
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
