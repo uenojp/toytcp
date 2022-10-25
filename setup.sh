4370d32e697fa7651fcbedc52af6277361a09018
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-10-25 23:25:22 +0900

    Check if ethtool command exists

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 #
 # https://github.com/teru01/toytcp/blob/b8ae46590011d528a63325e800fce142e16139fc/setup.sh
 #
-# Copyright 2020 Teruya Ono
+# Copyright 2020 Teruya Ono(Original Author)
+# Copyright 2020 Takahiro Ueno
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # 
@@ -12,6 +13,12 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 # reference: https://techblog.ap-com.co.jp/entry/2019/06/28/100439
+
+if ! command -v ethtool > /dev/null 2>&1; then
+    echo "need 'ethtool' (command not found)" >&2
+    echo "run 'apt install ethtool'" >&2
+    exit 1
+fi
 
 set -eux
 
