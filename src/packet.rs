080b2985f4c76bb59e83e73b5560c1efaa4ce7da
Takahiro Ueno <uenotakahiro.jp@gmail.com>
2022-10-29 03:11:25 +0900

    Refactor

--- src/packet.rs
+++ src/packet.rs
@@ -2,18 +2,116 @@ use crate::tcp::TCP_HEADER_SIZE;
 
 use pnet::packet::Packet;
 
+// 
+//     0                   1                   2                   3
+//     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |          Source Port          |       Destination Port        |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |                        Sequence Number                        |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |                    Acknowledgment Number                      |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |  Data |           |U|A|P|R|S|F|                               |
+//    | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
+//    |       |           |G|K|H|T|N|N|                               |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |           Checksum            |         Urgent Pointer        |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |                    Options                    |    Padding    |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//    |                             data                              |
+//    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// 
+//                             TCP Header Format
+//
+// ref. https://www.rfc-editor.org/rfc/rfc793, Figure 3
+//
 #[derive(Clone)]
 pub struct TcpPacket {
     buffer: Vec<u8>,
 }
 
 impl TcpPacket {
-    pub fn new(src_port: u16, dst_port: u16, flag: u8, payload: &[u8]) -> Self {
-        let mut buffer = vec![0; TCP_HEADER_SIZE + payload.len()];
-        buffer[0..2].copy_from_slice(&src_port.to_be_bytes());
-        buffer[2..4].copy_from_slice(&dst_port.to_be_bytes());
-        buffer[13] = flag;
-        Self { buffer }
+    pub fn new() -> Self {
+        Self {
+            buffer: vec![0; TCP_HEADER_SIZE],
+        }
+    }
+
+    pub fn set_source(&mut self, source: u16) {
+        self.buffer[0..2].copy_from_slice(&source.to_be_bytes());
+    }
+
+    pub fn set_destination(&mut self, destination: u16) {
+        self.buffer[2..4].copy_from_slice(&destination.to_be_bytes());
+    }
+
+    pub fn set_sequence(&mut self, sequence: u32) {
+        self.buffer[4..8].copy_from_slice(&sequence.to_be_bytes());
+    }
+
+    pub fn set_acknowlegement(&mut self, acknowlegement: u32) {
+        self.buffer[8..12].copy_from_slice(&acknowlegement.to_be_bytes());
+    }
+
+    pub fn set_data_offset(&mut self, data_offset: u8) {
+        self.buffer[12] |= data_offset << 4;
+    }
+
+    pub fn set_flag(&mut self, flag: u8) {
+        self.buffer[13] = flag;
+    }
+
+    pub fn set_window(&mut self, window: u16) {
+        self.buffer[14..16].copy_from_slice(&window.to_be_bytes());
+    }
+
+    pub fn set_checksum(&mut self, checksum: u16) {
+        self.buffer[16..18].copy_from_slice(&checksum.to_be_bytes());
+    }
+
+    pub fn set_payload(&mut self, payload: &[u8]) {
+        self.buffer.resize(TCP_HEADER_SIZE + payload.len(), 0);
+        self.buffer[TCP_HEADER_SIZE..TCP_HEADER_SIZE + payload.len()].copy_from_slice(payload);
+    }
+
+    pub fn source(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[0], self.buffer[1]])
+    }
+
+    pub fn destination(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[2], self.buffer[3]])
+    }
+
+    pub fn sequence(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[4],
+            self.buffer[5],
+            self.buffer[6],
+            self.buffer[7],
+        ])
+    }
+
+    pub fn acknowlegement(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[8],
+            self.buffer[9],
+            self.buffer[10],
+            self.buffer[11],
+        ])
+    }
+
+    pub fn flag(&self) -> u8 {
+        self.buffer[13]
+    }
+
+    pub fn window(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[14], self.buffer[15]])
+    }
+
+    pub fn checksum(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[16], self.buffer[17]])
     }
 }
 
--- src/socket.rs
+++ src/socket.rs
@@ -1,4 +1,4 @@
-use crate::packet::TcpPacket;
+use crate::{packet::TcpPacket, tcp::TCP_HEADER_SIZE};
 
 use std::net::{IpAddr, Ipv4Addr};
 
@@ -7,36 +7,48 @@ use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet_transport::{self, TransportChannelType, TransportProtocol, TransportSender};
 
 pub struct Socket {
-    src_addr: Ipv4Addr,
-    dst_addr: Ipv4Addr,
-    src_port: u16,
-    dst_port: u16,
+    source_address: Ipv4Addr,
+    destination_address: Ipv4Addr,
+    source_port: u16,
+    destination_port: u16,
     sender: TransportSender,
 }
 
 impl Socket {
     pub fn new(
-        src_addr: Ipv4Addr,
-        dst_addr: Ipv4Addr,
-        src_port: u16,
-        dst_port: u16,
+        source_address: Ipv4Addr,
+        destination_address: Ipv4Addr,
+        source_port: u16,
+        destination_port: u16,
     ) -> Result<Self> {
         let (sender, _) = pnet_transport::transport_channel(
             65535,
             TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Tcp)),
         )?;
         Ok(Self {
-            src_addr,
-            dst_addr,
-            src_port,
-            dst_port,
+            source_address,
+            destination_address,
+            source_port,
+            destination_port,
             sender,
         })
     }
 
     pub fn send_tcp_packet(&mut self, flag: u8, payload: &[u8]) -> Result<usize> {
-        let packet = TcpPacket::new(self.src_port, self.dst_port, flag, payload);
-        let len = self.sender.send_to(packet, IpAddr::V4(self.dst_addr))?;
+        let mut packet = TcpPacket::new();
+        packet.set_source(self.source_port);
+        packet.set_destination(self.destination_port);
+        packet.set_sequence(0);
+        packet.set_acknowlegement(0);
+        packet.set_data_offset((TCP_HEADER_SIZE / 4) as u8);
+        packet.set_flag(flag);
+        packet.set_window(0);
+        packet.set_checksum(0);
+        packet.set_payload(payload);
+
+        let len = self
+            .sender
+            .send_to(packet, IpAddr::V4(self.destination_address))?;
         Ok(len)
     }
 }
